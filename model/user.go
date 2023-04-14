20510b75d9e861224b1419cd8f629392380e2766
unknown <wulandariiratnaa@gmail.com>
2023-04-14 14:22:22 +0700

    mengubah folder model

--- model/user.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type User struct {
-	Id       uint   `json:"id" gorm:"primaryKey;autoIncrement;"`
-	Name     string `json:"name"`
-	Username string `json:"username" gorm:"index:idx_username,unique"`
-	Password string `json:"password"`
-}
