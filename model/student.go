3593cd5dc55490df67b94030153116c8bfffdc22
unknown <wulandariiratnaa@gmail.com>
2023-04-14 14:45:54 +0700

    tugas final unjuk ketrampilan

--- model/student.go
+++ model/student.go
@@ -1,11 +1,11 @@
 package model
 
 type Student struct {
-	Id           uint   `gorm:"primaryKey" json:"id"`
-	Name         string `json:"name"`
-	Age          int    `json:"age"`
-	Gender       string `json:"gender"`
-	Address      string `json:"address"`
-	ProgramStudy string `json:"programstudy"`
-	GPA          int    `json:"gpa"`
+	Id           uint    `gorm:"primaryKey" json:"id"`
+	Name         string  `json:"name"`
+	Age          int     `json:"age"`
+	Gender       string  `json:"gender"`
+	Address      string  `json:"address"`
+	ProgramStudy string  `json:"programstudy"`
+	GPA          float64 `json:"gpa,omitempty"`
 }
