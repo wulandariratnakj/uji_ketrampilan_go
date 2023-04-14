254d8620d910b2a9599f09e93ec5af2214b2f9e0
unknown <wulandariiratnaa@gmail.com>
2023-04-14 14:24:09 +0700

    mengubah file route.go

--- route/route.go
+++ route/route.go
@@ -8,9 +8,11 @@ import (
 
 func InitRoute() *echo.Echo {
 	e := echo.New()
-	e.POST("/users", controller.AddUsers)
-	e.GET("/users", controller.GetUsers)
-	e.GET("/users/:id", controller.GetDetailUsers)
+	e.POST("/student", controller.AddStudents)
+	e.GET("/student", controller.GetStudent)
+	e.GET("/student/:id", controller.GetDetailStudent)
+	e.DELETE("/student/:id", controller.DeleteStudent)
+
 	e.POST("/login", controller.Login)
 	return e
 }
